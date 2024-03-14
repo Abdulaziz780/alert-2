1a75cf2844ea21e01da5971ce3d1a875c3c4ca97
Abdulaziz780 <akhusanov410@gmail.com>
2024-03-14 14:03:20 +0500

    easy

---
+++ main.js
@@ -0,0 +1,6 @@
+let num1 = Math.round(Math.random() * 100)
+let num2 = 2
+
+alert(`hozir random sonni chiqaramis`)
+alert(`random sonimiz: ${num1}`)
+alert(` biza random soni 2 kopaydirdik: ${num1 * num2}`)
